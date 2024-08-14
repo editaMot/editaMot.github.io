536f7f5b3c292dfcf9feec91b4cf76d41fac5d99
Edita Motuze <edita.motuze@gmail.com>
2024-08-14 22:33:11 +0300

    feat: created components for sections

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+import styles from "./Section.module.scss";
+
+interface SectionProps {
+  title: string;
+  content: ReactNode;
+  isExpanded: boolean;
+  handleSectionClick: () => void;
+}
+
+export const Section: React.FC<SectionProps> = ({
+  title,
+  content,
+  isExpanded,
+  handleSectionClick,
+}) => {
+  return (
+    <section
+      className={`${styles.section} ${
+        isExpanded ? styles["section--expanded"] : styles["section--collapsed"]
+      }`}
+      onClick={handleSectionClick}
+    >
+      {!isExpanded && <p className={styles.section__title}>{title}</p>}
+      {isExpanded && content}
+    </section>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./Button/Button";
 export * from "./Header/Header";
 export * from "./Main/Main";
+export * from "./Section/Section";
 export * from "./Tabs/Tabs";
