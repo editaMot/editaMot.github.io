99820673ad14be3aa0b74d6d15894ce8ec9a0202
Edita Motuze <edita.motuze@gmail.com>
2024-08-15 23:13:17 +0300

    feat: created card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,58 @@
+import { MdOutlineArrowOutward } from "react-icons/md";
+import { Button } from "../Button/Button";
+import styles from "./Card.module.scss";
+
+interface Tool {
+  title: string;
+  image: string;
+}
+
+interface CardProps {
+  title: string;
+  liveUrl: string;
+  gitUrl: string;
+  image: string;
+  tools: Tool[];
+}
+
+export const Card: React.FC<CardProps> = ({
+  title,
+  liveUrl,
+  gitUrl,
+  image,
+  tools,
+}) => {
+  return (
+    <div className={styles.card}>
+      <img
+        src={image}
+        alt={title}
+        loading="lazy"
+        className={styles.card__image}
+      />
+
+      <div className={styles.card__content}>
+        <h3 className={styles.card__heading}>{title}</h3>
+        <div className={styles.card__tools}>
+          {tools.map(({ title, image }) => (
+            <img src={image} alt={title} loading="lazy" />
+          ))}
+        </div>
+        <div className={styles.card__buttons}>
+          <Button
+            text="Live"
+            icon={<MdOutlineArrowOutward />}
+            action={() => window.open(liveUrl, "_blank")}
+            type="text"
+          />
+          <Button
+            text="GitHub"
+            action={() => window.open(gitUrl, "_blank")}
+            type="text"
+            icon={<MdOutlineArrowOutward />}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
