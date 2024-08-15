b6e43cd023effbd33613c5bbcba6afd81eaf786d
Edita Motuze <edita.motuze@gmail.com>
2024-08-15 23:15:14 +0300

    update: created new button type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,23 @@
+import { ReactNode } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
   text: string;
   action?: () => void;
-  type?: "primary" | "outlined";
+  type?: "primary" | "outlined" | "text";
+  icon?: ReactNode;
 }
 
 export const Button: React.FC<ButtonProps> = ({
   text,
   action,
   type = "primary",
+  icon,
 }) => (
   <button
     onClick={action}
     className={`${styles.btn} ${styles[`btn--${type}`]}`}
   >
-    {text}
+    {text} {icon && <span className={styles.btn__icon}>{icon}</span>}
   </button>
 );
