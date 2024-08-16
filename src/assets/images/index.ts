39fe801cc4792136b60301e1c8610cea877f8eb9
Edita Motuze <edita.motuze@gmail.com>
2024-08-16 20:00:28 +0300

    feat: created component for skills list

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,37 +1,72 @@
 import aesthetic from "./aesthetic.jpg";
 import audiophile from "./audiophile.jpg";
+import css from "./CSS.svg";
 import express from "./exppress.png";
+import expressDark from "./ExpressJS-Dark.svg";
+import figma from "./Figma-Dark.svg";
+import firebaseDark from "./Firebase-Dark.svg";
 import firebase from "./firebase.png";
 import form from "./form.png";
 import framer from "./framer-motion.png";
+import git from "./Git.svg";
+import html from "./HTML.svg";
+import js from "./JavaScript.svg";
+import mui from "./MaterialUI-Dark.svg";
 import mongodb from "./mongodb.png";
+import mongodbDark from "./MongodbDark.svg";
 import mountainCabin from "./mountain_cabin.jpg";
 import natureLovers from "./nature-lovers.jpg";
 import query from "./query.png";
+import react from "./React-Dark.svg";
+import formDark from "./react-hook-form.svg";
 import redux from "./redux.png";
+import reduxDark from "./Redux.svg";
 import router from "./router.png";
+import sassDark from "./Sass.svg";
 import scss from "./scss.png";
-import supabase from "./supabase.png";
+import styledComponent from "./StyledComponents.svg";
+import { default as supabase, default as supabaseDark } from "./supabase.png";
+import tailwind from "./TailwindCSS-Dark.svg";
 import ts from "./ts.png";
+import viteDark from "./Vite-Dark.svg";
 import vite from "./vite.png";
+import vitestDark from "./Vitest-Dark.svg";
 import vitest from "./vitest.png";
 
 export {
   aesthetic,
   audiophile,
+  css,
   express,
+  expressDark,
+  figma,
   firebase,
+  firebaseDark,
   form,
+  formDark,
   framer,
+  git,
+  html,
+  js,
   mongodb,
+  mongodbDark,
   mountainCabin,
+  mui,
   natureLovers,
   query,
+  react,
   redux,
+  reduxDark,
   router,
+  sassDark,
   scss,
+  styledComponent,
   supabase,
+  supabaseDark,
+  tailwind,
   ts,
   vite,
+  viteDark,
   vitest,
+  vitestDark,
 };
---
+++ src/pages/Home/components/Skills/SkillsList/SkillList.tsx
@@ -0,0 +1,22 @@
+import { SkillsItem } from "../../../../../data/skills";
+import styles from "./SkillList.module.scss";
+
+interface SkillsListProp {
+  skills: SkillsItem[];
+  heading: string;
+}
+
+export const SkillsList: React.FC<SkillsListProp> = ({ skills, heading }) => {
+  return (
+    <div className={styles.skills}>
+      <h3 className={styles.skills__heading}>{heading}</h3>
+      <ul className={styles.skills__list}>
+        {skills.map(({ title, image }) => (
+          <li key={title} className={styles.skills__item}>
+            <img src={image} alt={title} loading="lazy" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
