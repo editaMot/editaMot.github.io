4fdfafa0eac52461934596bc36dc5a30a3cb320d
Edita Motuze <edita.motuze@gmail.com>
2024-08-18 19:07:18 +0300

    feat: created component for social media

---
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
+import styles from "./SocialMedia.module.scss";
+
+interface SocialMedia {
+  title: string;
+  image: ReactNode;
+  url: string;
+}
+
+const SOCIAL_MEDIA: SocialMedia[] = [
+  {
+    title: "LinkedIn",
+    image: <FaLinkedin />,
+    url: "https://www.linkedin.com/in/edita-motuze",
+  },
+  {
+    title: "GitHub",
+    image: <FaSquareGithub />,
+    url: "https://github.com/editaMot",
+  },
+];
+
+export const SocialMedia: React.FC = () => {
+  return (
+    <ul className={styles.socialMedia}>
+      {SOCIAL_MEDIA.map(({ title, image, url }) => (
+        <li key={title} className={styles.socialMedia__item}>
+          <a href={url}>{image}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
